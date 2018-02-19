20c52ec2f82fbdebb74624c2ef4e6a91fa7c3a39
hli30 <harrylll93@gmail.com>
2018-02-19 22:55:57 +0000

    Chnaged callback to log out avatar_url of contributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,24 @@
 var request = require('request');
+var secrets = require('./secrets');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN
+    }
+  };
 
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors("hli30", "MTClicker", function(err, result) {
+    console.log("Errors:", err);
+    result = JSON.parse(result);
+    result.forEach(function(data) {
+        console.log(data.avatar_url);
+    });
 });
